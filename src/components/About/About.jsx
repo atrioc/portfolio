061a3cb168c5d254378a5dbacc5570ee796d6c14
kunalw <kunalwasnik54@gmail.com>
2022-09-27 11:20:28 +0530

    aster

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,40 +11,31 @@ const About = () => {
     <>
     <div id="about" className='cards'>
       <div className="a-left">
-        <motion.div 
-        initial={{ left:"-22rem" }}
-        whileInView={{ left:"-11%" }}
-        transition={{duration:1,type:"spring"}}>
+        
           <Card
           head={"UX/UI"} 
           details={" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum consequat nisl vel pretium lectus quam. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Eget sit amet tellus cras adipiscing enim eu turpis."}
           />
-        </motion.div>
+        
           <span>About Me..</span>
           
       </div>
       {/* <img className="dd" src={dd} alt="sd"></img> */}
       <div className="a-middle">
-      <motion.div 
-        initial={{ left:"-22rem" }}
-        whileInView={{ left:"-11rem" }}
-        transition={transition}>
+      
        <Card 
        head={"Data Science"} 
        details={" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum consequat nisl vel pretium lectus quam. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Eget sit amet tellus cras adipiscing enim eu turpis."} 
              />
-      </motion.div>
+      
       </div>
       <div className='a-right'>
-      <motion.div 
-        initial={{ left: "14rem" }}
-        whileInView={{ right: "11rem" }}
-        transition={transition}>
+      
          <Card 
            head={"Frontend Development"} 
            details={" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum consequat nisl vel pretium lectus quam. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Eget sit amet tellus cras adipiscing enim eu turpis."}
          />
-      </motion.div>
+      
         </div>
        </div>
   
